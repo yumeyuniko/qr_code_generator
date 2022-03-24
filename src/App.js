e153adda5094b88158b60f12e249fe6ca67d40d9
yumeyuniko <daigocafe@msn.com>
2022-03-24 16:04:21 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
+import axios from 'axios';
+import { createContext, useState } from 'react';
+import InputForm from './components/InputForm';
+import QrCode from './components/QrCode';
+
+//Create context
+export const InputContext = createContext();
+
 function App() {
+  const [inputValue, setInputValue] = useState({
+    url: '',
+    color: '',
+  });
+
+  const [response, setResponse] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const config = {
+    headers: { Authorization: 'Bearer 5e9aa120-aad9-11ec-837d-b9aba815bd98' },
+  };
+
+  const bodyParameters = {
+    colorDark: inputValue.color,
+    qrCategory: 'url',
+    text: inputValue.url,
+  };
+
+  const getQrCode = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        'https://qrtiger.com/api/qr/static',
+        bodyParameters,
+        config
+      );
+      setResponse(res.data.url);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    inputValue,
+    setInputValue,
+    getQrCode,
+    response,
+    loading,
+    error,
+  };
+
   return (
     <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 px2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
         <div className="md:grid grid-cols-3">
-          <h1>Input</h1>
-          <h1>qrCode</h1>
+          <InputContext.Provider value={value}>
+            <InputForm />
+            <QrCode />
+          </InputContext.Provider>
         </div>
       </div>
     </div>
